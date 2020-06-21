268ae9ca4432cb5fd9c1d30e2fabb5f8a9944a0b
Maricela Romero <mariromero@MacBook-Pro-de-Maricela.local>
2020-06-21 17:10:19 -0500

    Tarea Programa Arreglalo

--- Arreglalo.java
+++ Arreglalo.java
@@ -1,20 +1,29 @@
 public class Arreglalo {
-	public static void main() {
-		Cuadrado cuadrado = Cuadrado();
+
+	public static void main( String args []) {
+		Cuadrado cuadrado = new Cuadrado();
 
 		cuadrado.figura.rodar();
+	}
+}
+class Figura{
+	String nombre;
+
+	public void rodar()
+	{
+		System.out.println("Rodando como: "+nombre);
 	}
 }
 
 class Cuadrado {
 
-	Figura figura;
+	Figura figura = new Figura();
 
 	public Cuadrado() {
 		iniciar();
 	}
 
 	private void iniciar() {
-		this.figura.nombre = cuadrado;
+		this.figura.nombre = "cuadrado";
 	}
 }
