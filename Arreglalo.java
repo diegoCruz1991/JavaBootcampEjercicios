1c4e2158c7b89bc816844dbef56aff3d00737260
Eder Montano <eder.montano@astrazeneca.com>
2020-06-21 01:32:06 -0500

    Eder ejercicio Arreglalo.java

--- Arreglalo.java
+++ Arreglalo.java
@@ -1,20 +1,16 @@
-public class Arreglalo {
-	public static void main() {
-		Cuadrado cuadrado = Cuadrado();
-
-		cuadrado.figura.rodar();
+public class Arreglalo{
+	public static void main(String... args) {
+		Cuadrado cuadrado = new Cuadrado();
+		System.out.println(cuadrado.nombre);
 	}
 }
 
-class Cuadrado {
-
-	Figura figura;
-
+class Cuadrado{
+	String nombre;
 	public Cuadrado() {
 		iniciar();
 	}
-
 	private void iniciar() {
-		this.figura.nombre = cuadrado;
+		this.nombre = "Cuadrado";
 	}
 }
