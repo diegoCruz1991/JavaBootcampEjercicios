5d32449a1078c32b879ed60ab7375b29368cabf9
Maozans <elfensunrise@gmail.com>
2020-06-22 11:41:57 -0500

    Practica1 Java Arreglalo

--- Arreglalo.java
+++ Arreglalo.java
@@ -1,20 +1,16 @@
 public class Arreglalo {
-	public static void main() {
-		Cuadrado cuadrado = Cuadrado();
-
-		cuadrado.figura.rodar();
+	public static void main(String[] args) {
+		Cuadrado cuadrado = new Cuadrado();
 	}
 }
 
 class Cuadrado {
-
-	Figura figura;
-
 	public Cuadrado() {
 		iniciar();
 	}
-
-	private void iniciar() {
-		this.figura.nombre = cuadrado;
+	String nombre;
+	public void iniciar() {
+		this.nombre = "cuadrado";
+		System.out.println("Soy un cuadradooooo");
 	}
 }
