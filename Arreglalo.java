39ec1c526d356ec1048b6c7e66cd3e66056d960d
MARIANTY DANIELA DELGADO MAYORA <mddelgadom@ad.elektra.com.mx>
2020-06-22 11:33:43 -0500

    subiendo el cambio

--- Arreglalo.java
+++ Arreglalo.java
@@ -1,14 +1,15 @@
 public class Arreglalo {
-	public static void main() {
-		Cuadrado cuadrado = Cuadrado();
+	public static void main(String... args) {
+		
+		Cuadrado cuadrado = new Cuadrado();
 
 		cuadrado.figura.rodar();
 	}
 }
 
-class Cuadrado {
-
-	Figura figura;
+class Cuadrado extends Figura {
+	Figura figura= new Figura();
+	String cuadrado;
 
 	public Cuadrado() {
 		iniciar();
@@ -17,4 +18,18 @@ class Cuadrado {
 	private void iniciar() {
 		this.figura.nombre = cuadrado;
 	}
+	
+	
+}
+
+class Figura{
+String nombre;
+
+Figura(){
+	this.nombre=nombre;
+}	
+	public void rodar(){
+		System.out.println("Esta figura pudo rodar...");
+	}
+	
 }
