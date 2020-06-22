32dd7988026e614e00935527caa0856487ae565d
Your NameCynthia <cynthia.urbano.romero@gmail.com>
2020-06-22 10:28:49 -0500

    Tarea Arreglalo de Cynthia

--- Arreglalo.java
+++ Arreglalo.java
@@ -1,20 +1,23 @@
 public class Arreglalo {
-	public static void main() {
-		Cuadrado cuadrado = Cuadrado();
 
-		cuadrado.figura.rodar();
+	public static void main(String... args) {
+	
+		Cuadrado cuadrado = new Cuadrado();
+ 		System.out.println("Esto es un "+ cuadrado.figura);
+	
 	}
 }
 
-class Cuadrado {
 
-	Figura figura;
+class Cuadrado {
+		
+	String figura;
 
 	public Cuadrado() {
 		iniciar();
 	}
 
-	private void iniciar() {
-		this.figura.nombre = cuadrado;
+	private void  iniciar() {
+		this.figura = "Cuadrado";
 	}
 }
