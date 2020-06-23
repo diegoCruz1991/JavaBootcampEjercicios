bbfd2be86ad5ffee40f78a9fb2f0586ca29e81a8
Maozans <elfensunrise@gmail.com>
2020-06-22 20:16:18 -0500

    Practica2 Arreglalo2

--- Arreglalo2.java
+++ Arreglalo2.java
@@ -2,15 +2,17 @@ public class Arreglalo2 {
 	public Persona persona;
 
 	public static void main(String... args) {
-		Calculadora calculadora = persona.getCalculadora();
+		Arreglalo2 arreglalo2 = new Arreglalo2();
+		arreglalo2.persona = new Persona();
+		Calculadora calculadora = arreglalo2.persona.getCalculadora();
 
-		short resultadoSuma = calculadora.sumar('10', 5);
+		int resultadoSuma = calculadora.sumar(10, 5);
 
-		System.out.println(resultado);
+		System.out.println(resultadoSuma);
 
-		short resultadoDividir = calculadora.dividir(5.5f, 4.0f);
+		float resultadoDividir = calculadora.dividir(5.5f, 4.0f);
 
-		System.out.println(resultado);
+		System.out.println(resultadoDividir);
 	}
 }
 
@@ -21,7 +23,7 @@ class Persona {
 		this.calculadora = new Calculadora();
 	}
 
-	private Calculadora getCalculadora() {
+	public Calculadora getCalculadora() {
 		return this.calculadora;
 	}
 }
@@ -32,7 +34,7 @@ class Calculadora {
 		return a + b;
 	}
 
-	public float dividir(double numero1, double numero2) {
+	public float dividir(float numero1, float numero2) {
 		return numero1 / numero2;
 	}
 }
