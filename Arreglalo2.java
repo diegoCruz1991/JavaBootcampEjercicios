162c1904b508be7ae448def9fc2b100617b70bac
Mauricio <mauricio_berumen@epam.com>
2020-06-22 20:17:49 -0500

    ejercicio corregido

---
+++ Arreglalo2.java
@@ -0,0 +1,42 @@
+public class Arreglalo2 {
+	public Persona persona;
+	
+	public static void main(String... args) {
+		Arreglalo2 arreglalo2 = new Arreglalo2();
+		
+		arreglalo2.persona = new Persona();
+		
+		Calculadora calculadora = arreglalo2.persona.getCalculadora();
+		
+		int resultadoSuma = calculadora.sumar(10, 5);
+
+		System.out.println(resultadoSuma);
+
+		float resultadoDividir = calculadora.dividir(5.5f, 4.0f);
+
+		System.out.println(resultadoDividir);
+	}
+}
+
+class Persona {
+	private Calculadora calculadora;
+
+	Persona() {
+		this.calculadora = new Calculadora();
+	}
+
+	public Calculadora getCalculadora() {
+		return this.calculadora;
+	}
+}
+
+class Calculadora {
+
+	public int sumar(int a, int b) {
+		return a + b;
+	}
+
+	public float dividir(float numero1, float numero2) {
+		return numero1 / numero2;
+	}
+}
