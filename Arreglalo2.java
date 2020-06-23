fcedc8415e22f737d75925dbd6074995c39501bf
MARIANTY DANIELA DELGADO MAYORA <mddelgadom@ad.elektra.com.mx>
2020-06-22 20:24:33 -0500

    subiendo...

--- Arreglalo2.java
+++ Arreglalo2.java
@@ -2,37 +2,39 @@ public class Arreglalo2 {
 	public Persona persona;
 
 	public static void main(String... args) {
-		Calculadora calculadora = persona.getCalculadora();
+		Arreglalo2 arre= new Arreglalo2();
+	  arre.persona=new Persona();
+		Calculadora calculadora = arre.persona.getCalculadora();
 
-		short resultadoSuma = calculadora.sumar('10', 5);
+		int resultadoSuma = calculadora.sumar(10, 5);
 
-		System.out.println(resultado);
+		System.out.println(resultadoSuma);
 
-		short resultadoDividir = calculadora.dividir(5.5f, 4.0f);
+		float resultadoDividir = calculadora.dividir(5.5f, 4.0f);
 
-		System.out.println(resultado);
+		System.out.println(resultadoDividir);
 	}
 }
 
-class Persona {
+class Persona extends Calculadora{
 	private Calculadora calculadora;
 
 	Persona() {
 		this.calculadora = new Calculadora();
 	}
 
-	private Calculadora getCalculadora() {
+	public Calculadora getCalculadora() {
 		return this.calculadora;
 	}
 }
 
-class Calculadora {
-
+class Calculadora{
+	
 	public int sumar(int a, int b) {
 		return a + b;
 	}
 
-	public float dividir(double numero1, double numero2) {
+	public float dividir(float numero1, float numero2) {
 		return numero1 / numero2;
 	}
 }
