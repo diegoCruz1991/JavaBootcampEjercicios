955a42e50507c9d066709b00dceaf60c03bd316e
Carlos Rodriguez <jcgarj@gmail.com>
2020-06-22 20:32:22 -0500

    Arrelgalo2

--- Arreglalo2.java
+++ Arreglalo2.java
@@ -2,37 +2,45 @@ public class Arreglalo2 {
 	public Persona persona;
 
 	public static void main(String... args) {
-		Calculadora calculadora = persona.getCalculadora();
+		Arreglalo2 arreglalo2 = new Arreglalo2();
+		
+		arreglalo2.persona = new Persona();
+		
+		Calculadora calculadora = arreglalo2.persona.getCalculadora();
 
-		short resultadoSuma = calculadora.sumar('10', 5);
+		int resultadoSuma = calculadora.sumar(10, 5);
 
-		System.out.println(resultado);
+		System.out.println(resultadoSuma);
 
-		short resultadoDividir = calculadora.dividir(5.5f, 4.0f);
+		Float resultadoDividir = calculadora.dividir(5.5f, 4.0f);
 
-		System.out.println(resultado);
+		System.out.println(resultadoDividir);
 	}
 }
 
 class Persona {
-	private Calculadora calculadora;
+	public Calculadora calculadora;
 
 	Persona() {
 		this.calculadora = new Calculadora();
 	}
 
-	private Calculadora getCalculadora() {
+	public Calculadora getCalculadora(){
 		return this.calculadora;
 	}
+
+	
 }
 
 class Calculadora {
-
-	public int sumar(int a, int b) {
-		return a + b;
+ 
+	 public int sumar(int a, int b) {
+	 	return  a + b;
 	}
 
-	public float dividir(double numero1, double numero2) {
+	 public float dividir(float numero1,float numero2 ) {
+	 	
 		return numero1 / numero2;
+		
 	}
 }
