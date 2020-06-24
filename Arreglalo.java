4ff06b040eea4316b7594ba4456f317bd10c4a38
Veronica <veronica.garcia1187@gmail.com>
2020-06-23 20:24:28 -0400

    Ejercicio clase

---
+++ Arreglalo.java
@@ -0,0 +1,20 @@
+public class Arreglalo {
+	public static void main(String... args) {
+		Cuadrado cuadrado = new Cuadrado();
+		cuadrado.iniciar();
+
+	}
+}
+
+class Cuadrado {
+			Cuadrado cuadrado;
+
+		public void iniciar() {
+			System.out.println("Estoy iniciando como un Cuadrado");
+			this.cuadrado = cuadrado;
+
+		}
+
+	}
+
+
