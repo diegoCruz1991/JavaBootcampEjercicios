314110227e0d09e838197d48940846e7e5a67398
Your NameCynthia <cynthia.urbano.romero@gmail.com>
2020-06-23 20:07:59 -0500

    Ejercicio Arreglalo2

--- Arreglalo2.java
+++ Arreglalo2.java
@@ -2,37 +2,38 @@ public class Arreglalo2 {
 	public Persona persona;
 
 	public static void main(String... args) {
-		Calculadora calculadora = persona.getCalculadora();
+		Arreglalo2 arreglalo2=new Arreglalo2 ();
+		arreglalo2.persona= new Persona(); ///Nuevo
+		Calculadora calculadora = arreglalo2.persona.getCalculadora();
 
-		short resultadoSuma = calculadora.sumar('10', 5);
+		int resultadoSuma = calculadora.sumar(10, 5); //Aqui en el 10 
 
-		System.out.println(resultado);
+		System.out.println(resultadoSuma);
 
-		short resultadoDividir = calculadora.dividir(5.5f, 4.0f);
+		float resultadoDividir = calculadora.dividir(5.5f, 4.0f); //aqui
 
-		System.out.println(resultado);
+		System.out.println(resultadoDividir);
 	}
 }
 
 class Persona {
 	private Calculadora calculadora;
 
-	Persona() {
+	Persona() { //aqui
 		this.calculadora = new Calculadora();
 	}
 
-	private Calculadora getCalculadora() {
+	public Calculadora getCalculadora() { //Se vuelve publico
 		return this.calculadora;
 	}
 }
-
 class Calculadora {
 
 	public int sumar(int a, int b) {
 		return a + b;
 	}
 
-	public float dividir(double numero1, double numero2) {
+	public float dividir(float numero1, float numero2) {
 		return numero1 / numero2;
 	}
 }
