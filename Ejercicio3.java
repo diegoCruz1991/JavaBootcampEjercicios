2682035042a262ef6efc71a50bbd45e4a15f04ec
MARIANTY DANIELA DELGADO MAYORA <mddelgadom@ad.elektra.com.mx>
2020-06-23 22:18:17 -0500

    subiendo cambios...

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -1,18 +1,54 @@
 public class Ejercicio3 {
 
 	public static void main(String... args) {
+		
+			
 		Calculadora calculadora = new Calculadora();
-		//Hacer que el metodo sumar() de calculadora me traiga la suma de 2 numeros enteros
-		//Hacer que el metodo restar() de calculadora me traiga la suma de 2 numeros enteros
-		//Hacer que el metodo dividir() de calculadora me traiga la division de 2 numeros double
-		//Hacer que el metodo multiplicar() de calculadora me traiga la multiplicacion de 2 numeros flotantes
+	 int resultadoSuma=calculadora.sumar(23, 62);
+	 System.out.println("El resultado de la suma de dos numeros enteros fue:"+resultadoSuma);
+	 
+	 int resultadoResta=calculadora.restar(70, 59);
+	System.out.println("El resultado de la resta de dos numeros enteros fue:"+resultadoResta);
+	
+	double resultadoDivision=calculadora.dividor(25, 6);
+	System.out.println("El resultado de la division fue:"+resultadoDivision);
+
+	double resultadoMultiplicacion=calculadora.multiplicar(6, 8);
+	System.out.println("El resultado de la multiplicacion es:"+resultadoMultiplicacion);
 		CalculadoraLogica calculadoraLogica = new CalculadoraLogica();
 
-		boolean esIgual = calculadoraLogica.esIgual(5 / 5 + 4 - 3 * 7 + 10, 18);
-		/*
-			Primero, crear el metodo esIgual para que compare que 2 numeros son iguale so no
-			Segundo, hacer que el metodo esIgual regrese true cuando le paso los valores y/o operaciones que estan arriba
-		*/
+	boolean esIgual = calculadoraLogica.esIgual(5 / 5 + 4 - 3 * 7 + 10, 18);
+	System.out.println(esIgual);
+	
 		
+	}
+}
+
+class Calculadora{
+
+
+		public int sumar(int nro1,int nro2){
+			return nro1+nro2;
+		}
+
+		public int restar(int nro1,int nro2){
+			return nro1-nro2;
+		}
+	
+		public double multiplicar(double d1,double d2){
+			return d1*d2;
+		}
+		public double dividor(double d1,double d2){
+			return d1/d2;
+		}
+		
+		
+}
+
+class CalculadoraLogica extends Calculadora{
+	public boolean esIgual(int na, int nb){
+		na=na*(-3);
+		if(na==nb) return true;
+		else return false;
 	}
 }
