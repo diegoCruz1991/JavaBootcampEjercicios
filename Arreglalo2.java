8cc5e1aab2682c5b1c377faecdc0f08999a2db03
Bernardo Cabrera <berna_cabrera@hotmail.com>
2020-06-24 18:05:52 -0500

    Arreglalo2 de Bernardo

--- Arreglalo2.java
+++ Arreglalo2.java
@@ -1,43 +1,40 @@
-public class Arreglalo2 {
-	public Persona persona;
+public class Ejercicio3 {
 
-	public static void main(String... args) {
-		Arreglalo2 arreglalo2 = new Arreglalo2();
+    public static void main(String... args) {
+        class Calculadora(); 
 
-		arreglalo2.persona = new Persona();
+        Calculadora calculadora = new Calculadora();
 
-		Calculadora calculadora = arreglalo2.persona.getCalculadora();
-		
-		int resultadoSuma = calculadora.sumar(10, 5);
+        int a = 2;
+        int b = 4;
+        double c = 6.5d;
+        double d = 8.1d;
+        float e = 5.4f;
+        float f = 3.2f;
 
-		System.out.println(resultadoSuma);
+        int sumar = a + b;
+        int restar = a - b;
+        float multiplicar = e * f;
+        double dividir = c / d;
 
-		float resultadoDividir = calculadora.dividir(5.5f, 4.0f);
+                System.out.println(sumar);
+                System.out.println(restar);
+                System.out.println(multiplicar);
+                System.out.println(dividir);
+    class CalculadoraLogica();
+    esIgual = (5 / 5 + 4 - 3 * 7 + 10, 18); {
+                //Hacer que el metodo sumar() de calculadora me traiga la suma de 2 numeros enteros
+                //Hacer que el metodo restar() de calculadora me traiga la suma de 2 numeros enteros
+                //Hacer que el metodo dividir() de calculadora me traiga la division de 2 numeros double
+                //Hacer que el metodo multiplicar() de calculadora me traiga la multiplicacion de 2 numeros flotantes
+                CalculadoraLogica calculadoraLogica = new CalculadoraLogica();
 
-		System.out.println(resultadoDividir);
-	}
-}
+                boolean esIgual = calculadoraLogica.esIgual(5 / 5 + 4 - 3 * 7 + 10, 18);
+            {
+		/*
+			Primero, crear el metodo esIgual para que compare que 2 numeros son iguale so no
+			Segundo, hacer que el metodo esIgual regrese true cuando le paso los valores y/o operaciones que estan arriba
+		*/
 
-class Persona {
-	private Calculadora calculadora;
 
-	Persona() {
-		this.calculadora = new Calculadora();
-	}
-
-	public Calculadora getCalculadora() {
-		return this.calculadora;
-	}
-}
-
-class Calculadora {
-	
-	public int sumar(int a, int b) {
-		return a + b;
-	}
-
-	public float dividir(float numero1 , float numero2) {
-		return numero1 / numero2;
-	
-	}
-}
+            }
