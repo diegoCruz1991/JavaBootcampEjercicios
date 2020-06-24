33de709a60152ad59710462f6e3ad26c7434832b
Maricela Romero <mariromero@MacBook-Pro-de-Maricela.local>
2020-06-23 19:07:24 -0500

    tarea ejercicio3

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -1,18 +1,54 @@
+
 public class Ejercicio3 {
 
+	
 	public static void main(String... args) {
 		Calculadora calculadora = new Calculadora();
-		//Hacer que el metodo sumar() de calculadora me traiga la suma de 2 numeros enteros
-		//Hacer que el metodo restar() de calculadora me traiga la suma de 2 numeros enteros
-		//Hacer que el metodo dividir() de calculadora me traiga la division de 2 numeros double
-		//Hacer que el metodo multiplicar() de calculadora me traiga la multiplicacion de 2 numeros flotantes
+		int resultadoSuma = calculadora.sumar(10, 5);
+		System.out.println(resultadoSuma);
+
+		int resultadoResta = calculadora.restar(15, 5);
+		System.out.println(resultadoResta);
+
+		double resultadoDivide= calculadora.dividir(2.5, 0.5);
+		System.out.println(resultadoDivide);
+
+		float resultadoMultiplica = calculadora.multiplicar(234.5f, 1.1f);
+		System.out.println(resultadoMultiplica);
+//
 		CalculadoraLogica calculadoraLogica = new CalculadoraLogica();
 
 		boolean esIgual = calculadoraLogica.esIgual(5 / 5 + 4 - 3 * 7 + 10, 18);
-		/*
-			Primero, crear el metodo esIgual para que compare que 2 numeros son iguale so no
-			Segundo, hacer que el metodo esIgual regrese true cuando le paso los valores y/o operaciones que estan arriba
-		*/
+		calculadoraLogica.esIgual(5 / 5 + (4 - 3) * (7 + 10), 18);
 		
 	}
+
+}
+ class Calculadora {
+
+	public int sumar(int a, int b) {
+		return a + b;
+	}
+
+	public int restar(int a, int b){
+		return a - b;
+	}
+
+	public double dividir(double numero1, double numero2) {
+		return numero1 / numero2;
+	}
+
+	public float multiplicar (float numero1, float numero2) {
+		return numero1 * numero2;
+	}
+}
+
+class CalculadoraLogica{
+
+	boolean esIgual(int value1, int value2)
+	{
+		System.out.println("esIgual value1: "+value1+" y value2: "+ value2 + (value1 == value2 ? " Si":" No"));
+		return value1 == value2;
+	}
+
 }
