a16d705971771fb98c8deca241ea4b73b9567ed6
Alberto Picasso <picassoa12@gmail.com>
2020-06-25 10:15:01 -0500

    Ejercicio 3 24_06_2020

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -8,11 +8,44 @@ public class Ejercicio3 {
 		//Hacer que el metodo multiplicar() de calculadora me traiga la multiplicacion de 2 numeros flotantes
 		CalculadoraLogica calculadoraLogica = new CalculadoraLogica();
 
+		System.out.println("El resultado de la suma es: " + calculadora.sumar(13,14));
+		System.out.println("El resultado de la resta es: " + calculadora.restar(24,13));
+		System.out.println("El resultado de la division es: " + calculadora.dividir(13.456f,4.543f));
+		System.out.println("El resultado de la multiplicacion es: " + calculadora.multiplicar(23.231f,43.231f));
+
+
 		boolean esIgual = calculadoraLogica.esIgual(5 / 5 + 4 - 3 * 7 + 10, 18);
+
+		System.out.println("El resultado de la operacion booleana es: " + esIgual);
 		/*
 			Primero, crear el metodo esIgual para que compare que 2 numeros son iguale so no
 			Segundo, hacer que el metodo esIgual regrese true cuando le paso los valores y/o operaciones que estan arriba
 		*/
 		
 	}
+}
+
+class Calculadora {
+	int sumar(int a, int b){
+		return a + b;
+	}
+
+	int restar(int c, int d){
+		return c - d;
+	}
+
+	float dividir(float e, float f){
+		return e / f;
+	}
+
+	float multiplicar(float g, float h){
+		return g * h;
+	}
+}
+
+class CalculadoraLogica {
+	boolean esIgual(int i, int j){
+		return i==j;
+	}
+
 }
