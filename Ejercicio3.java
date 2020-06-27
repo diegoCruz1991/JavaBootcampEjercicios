15c6c2f1a948df3f82781bb0f5376a765ac6872d
Antoreich <9704777+Antoreich@users.noreply.github.com>
2020-06-27 12:29:06 -0500

    Ejercicio 3 calculadora con sobrecarga

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -4,7 +4,7 @@ public class Ejercicio3 {
 	public static void main(String... args) {
 		Calculadora calculadora = new Calculadora();
 		//Hacer que el metodo sumar() de calculadora me traiga la suma de 2 numeros enteros
-		System.out.println(calculadora.sumar(5,5));
+		System.out.println(calculadora.sumar(2.3, 5, 10, 20, 30, 100.5));
 		//Hacer que el metodo restar() de calculadora me traiga la suma de 2 numeros enteros
 		System.out.println(calculadora.restar(10,5));
 		//Hacer que el metodo dividir() de calculadora me traiga la division de 2 numeros double
@@ -26,16 +26,112 @@ public class Ejercicio3 {
 }
 
 class Calculadora{
-	public int sumar(int a, int b){
+	public static int sumar(int a, int b){
 		return a+b;
 	}
-	public int restar(int a, int b){
+	public static double sumar(double a, double b){
+		return a+b;
+	}
+	public static float sumar(float a, float b){
+		return a+b;
+	}
+	public static int sumar(int ...a){
+		if (a.length <= 1) {
+			System.out.println("Esta suma es invalida");
+			return -1;
+		}
+		int suma=0;
+		for (int i=0; i< a.length; i++){
+			//System.out.println(a[i]);
+			suma += a[i];
+		}
+		return suma;
+	}
+	public static float sumar(float ...a){
+		if (a.length <= 1) {
+			System.out.println("Esta suma es invalida");
+			return -1;
+		}
+		float suma=0;
+		for (int i=0; i< a.length; i++){
+			//System.out.println(a[i]);
+			suma += a[i];
+		}
+		return suma;
+	}
+	public static double sumar(double ...a){
+		if (a.length <= 1) {
+			System.out.println("Esta suma es invalida");
+			return -1;
+		}
+		double suma=0;
+		for (int i=0; i< a.length; i++){
+			//System.out.println(a[i]);
+			suma += a[i];
+		}
+		return suma;
+	}
+
+	public static int restar(int a, int b){
 		return a-b;
 	}
-	public float dividir(float a, float b){
+	public static double restar(double a, double b){
+		return a-b;
+	}
+	public static float restar(float a, float b){
+		return a-b;
+	}
+	public static int restar(int ...a){
+		if (a.length <= 1) {
+			System.out.println("Esta resta es invalida");
+			return -1;
+		}
+		int resta=0;
+		for (int i=0; i< a.length; i++){
+			//System.out.println(a[i]);
+			resta -= a[i];
+		}
+		return resta;
+	}
+	public static float restar(float ...a){
+		if (a.length <= 1) {
+			System.out.println("Esta resta es invalida");
+			return -1;
+		}
+		float resta=0;
+		for (int i=0; i< a.length; i++){
+			//System.out.println(a[i]);
+			resta -= a[i];
+		}
+		return resta;
+	}
+	public static double restar(double ...a){
+		if (a.length <= 1) {
+			System.out.println("Esta resta es invalida");
+			return -1;
+		}
+		double resta=0;
+		for (int i=0; i< a.length; i++){
+			//System.out.println(a[i]);
+			resta -= a[i];
+		}
+		return resta;
+	}
+
+	public static float dividir(float a, float b){
 		return a/b;
 	}
-	public int multiplicar(int a, int b){
+	public static double dividir(double a, double b){
+		return a/b;
+	}
+
+	public static int multiplicar(int a, int b){
+		return a*b;
+	}
+	public static float multiplicar(float a, float b){
+		return a*b;
+	}
+	public static double multiplicar(double a, double b){
 		return a*b;
 	}
 }
@@ -51,3 +147,18 @@ class CalculadoraLogica{
 		}
 	}
 }
+
+/**
+ * Git steps to upload to remote branch
+ * 1 - git add Arreglalo2.java
+ * 2 - git commit -m "Arreglalo 2 terminado"
+ * 3 - git push -u origin LuisAntonio_branch
+ */
+
+/**
+ * Git steps to pull latest changes from master
+ * 1 - git checkout master  
+ * 2 - git pull origin master 
+ * 3 - git checkout LuisAntonio_branch
+ * 4 - git merge master
+ */
