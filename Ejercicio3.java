9514c5cb181e2ace1748ead73750e88eced18cc0
ARRS\drodriguez2 <dayana.rodriguez@commscope.com>
2020-06-27 12:33:11 -0500

    cambios clase 06/27/2020

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -11,17 +11,25 @@ public class Ejercicio3 {
 		boolean esIgual = calculadoraLogica.esIgual(5 / 5 + (4 - 3) * 7 + 10, 18);
 		System.out.println(esIgual);
 
-		int resultado1 = calculadora.sumar(1, 5);
-		System.out.println(resultado1);
+		//int resultado1 = calculadora.sumar(1, 5);
+		//System.out.println(resultado1);
 
-		int resultado2 = calculadora.restar(1, 5);
-		System.out.println(resultado2);
+		//int resultado2 = calculadora.restar(1, 5);
+		//System.out.println(resultado2);
 
-		double resultado3 = calculadora.dividir(5.5, 10.5);
-		System.out.println(resultado3);
+		//double resultado3 = calculadora.dividir(5.5, 10.5);
+		//System.out.println(resultado3);
 
-		float resultado4 = calculadora.multiplicar(1.5f, 5.5f);
-		System.out.println(resultado4);
+		//float resultado4 = calculadora.multiplicar(1.5f, 5.5f);
+		//System.out.println(resultado4);
+
+		System.out.println(calculadora.sumar(2.3, 5));
+		System.out.println(calculadora.sumar(3, 5.5));
+		System.out.println(calculadora.sumar(3.5, 5.5));
+		calculadora.sumar(2.3, 5, 10, 20);
+		calculadora.sumar(2.3);
+
+		// calculadora sumar o restar mas de dos numeros.
 
 		/*
 			Primero, crear el metodo esIgual para que compare que 2 numeros son iguales o no
@@ -42,19 +50,40 @@ public class Ejercicio3 {
 
 	class Calculadora {
 
-		public int sumar(int a, int b) {
+		public double sumar(double... muchosNumeros) {
+			double total = 0;
+			for(int x = 0; x < muchosNumeros.length; x++) {
+				total += muchosNumeros[x];
+			}
+			System.out.println(total);
+			return total;
+		}
+
+		public static int sumar(int a, int b) {
 			return a + b;
 		}
 
-		public int restar(int a, int b) {
+		public static double sumar(double a, int b) {
+			return a + b;
+		}
+
+		public static double sumar(int a, double b) {
+			return a + b;
+		}
+
+		public static double sumar(double a, double b) {
+			return a + b;
+		}
+
+		public static int restar(int a, int b) {
 			return a - b;
 		}
 
-		public double dividir(double x, double y) {
+		public static double dividir(double x, double y) {
 			return x / y;
 		}
 
-		public float multiplicar(float x, float y) {
+		public static float multiplicar(float x, float y) {
 			return x * y;
 		}
 	}
