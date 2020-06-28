17f1b95b2277080ba1e9ac80649a9b76dd95b2f0
Carlos Rodriguez <jcgarj@gmail.com>
2020-06-27 22:35:07 -0500

    Tarea 4 JC

---
+++ Tarea4.java
@@ -0,0 +1,22 @@
+package com.qaminds;
+
+import com.qaminds.App;
+
+public class Tarea4{
+
+	public static void main(String[] args){
+
+		System.out.println("Iniciando ejecucion");
+
+		App app1 = new App();
+		App app2 = new App();
+		App app3 = new App();
+		App app4 = new App();
+
+		app1.setAnio(2010);
+		app3.setMes(2);
+		app2.setDia(29);
+		app4.setFecha();
+	}
+}
+
