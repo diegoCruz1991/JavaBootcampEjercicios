8fc29edcabe6efe0e461e89154ecb81f2ff4dec6
Veronica <veronica.garcia1187@gmail.com>
2020-07-03 19:42:56 -0400

    nuevo cambio

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -6,8 +6,87 @@ public class Ejercicio3 {
 		//Hacer que el metodo restar() de calculadora me traiga la suma de 2 numeros enteros
 		//Hacer que el metodo dividir() de calculadora me traiga la division de 2 numeros double
 		//Hacer que el metodo multiplicar() de calculadora me traiga la multiplicacion de 2 numeros flotantes
+
+		CalculadoraLogica calculadoraLogica = new CalculadoraLogica();
+
+		boolean esIgual = calculadoraLogica.esIgual(((5/5)+((4-3)*7))+10, 18);
+		System.out.println(esIgual);
 		
-int a=6;
+		/*
+			Primero, crear el metodo esIgual para que compare que 2 numeros son iguale so no
+			Segundo, hacer que el metodo esIgual regrese true cuando le paso los valores y/o operaciones que estan arriba
+		*/	
+		
+	}
+}
+
+
+class Calculadora{
+
+	public int sumar(int a, int b){
+		return a + b;
+
+
+	}
+	public int restar(int a, int b){
+		return a - b;
+
+
+	}
+	public double dividir(double a, double b){
+		return a / b;
+
+
+	}
+	public float multiplicarr(float a, float b){
+		return a * b;
+
+
+	}
+
+}
+
+class CalculadoraLogica{
+
+	public boolean esIgual(int a, int b){
+		return a == b;
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&7
+
+
+/*public class Ejercicio3 {
+
+	public static void main(String... args) {
+		Calculadora calculadora = new Calculadora();
+		//Hacer que el metodo sumar() de calculadora me traiga la suma de 2 numeros enteros
+		//Hacer que el metodo restar() de calculadora me traiga la suma de 2 numeros enteros
+		//Hacer que el metodo dividir() de calculadora me traiga la division de 2 numeros double
+		//Hacer que el metodo multiplicar() de calculadora me traiga la multiplicacion de 2 numeros flotantes
+		
+int a=18;
 int b=9;
 int c=600;
 int d=600;
@@ -37,6 +116,7 @@ int d=600;
 			Segundo, hacer que el metodo esIgual regrese true cuando le paso los valores y/o operaciones que estan arriba
 		*/	
 		
+		/*
 	}
 }
 
@@ -61,7 +141,7 @@ class Resta extends Calculadora{
 
 class Multiplicacion extends Calculadora{
  
-    public int multiplicar(int a, int b){
+    public float multiplicar(float a, float b){
     	return a * b;
     	
 
@@ -85,6 +165,6 @@ class CalculadoraLogica{
     	
 
     }
- }
+ }*/
 
 
