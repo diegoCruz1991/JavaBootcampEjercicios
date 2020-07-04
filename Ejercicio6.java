3422aec25628cf6913d83a45da3beb5df9cda86d
Leys <papitasleys@aol.com>
2020-07-04 14:11:29 -0500

    Ejercicio 6, trim que quita espacios al inicio, en medio y final

---
+++ Ejercicio6.java
@@ -0,0 +1,13 @@
+//trim a  todos los espacios
+
+public class Ejercicio6 {
+  public static void main(String[] args) {
+    Ejercicio6 ej6 = new Ejercicio6();
+    System.out.println(ej6.trimEspecial("  quitar  espa ci os   "));
+
+  }
+
+  public String trimEspecial(String s){
+    return s.replace(" ","");
+  }
+}
