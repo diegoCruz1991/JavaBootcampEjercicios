e66b2198f1a1123268efa0e3121a90682745552c
Bernardo Cabrera <berna_cabrera@hotmail.com>
2020-07-06 12:42:24 -0500

    Ejercicio 6 Bernardo

---
+++ Ejercicio6.java
@@ -0,0 +1,12 @@
+public class Ejercicio6 {
+    public static void main (String[] args){
+        //Quitar espacio al inicio, enmedio y al final sin usar trim
+
+        String palabra ="  H   o    l      a       ";
+        System.out.println(palabra);
+        System.out.println(palabra.replace(" ",""));
+
+    }
+
+
+}
