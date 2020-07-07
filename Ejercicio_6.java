25fc7ab8feb27c49d2f8871c090a6536f8dedee9
maubman <67204720+maubman@users.noreply.github.com>
2020-07-06 19:49:51 -0500

    Add files via upload

---
+++ Ejercicio_6.java
@@ -0,0 +1,10 @@
+//Hacer un metodo trim especial
+public class Ejercicio_6 {
+	public static void main (String... args){
+		
+		String palabra = "  H    o    l    a   ";
+		System.out.println(palabra);
+		//No uusar el metodo trim
+		System.out.println(palabra.replace(" ",""));
+	}
+}
