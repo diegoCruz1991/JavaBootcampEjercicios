0d5d860b9d019b746b72d431daa0cd22a777d924
Cynthia Urbano <cynthia.urbano.romero@gmail.com>
2020-07-24 11:03:23 -0500

    Ejercicio quitar todos los espacios de una cadena

---
+++ trimSpecial.java
@@ -0,0 +1,15 @@
+public class trimSpecial{
+
+	public static void main(String... args) {
+
+		String cadena ="Hola mundo cadena"; //Se ingresa la cadena
+
+		String nueva=cadena.replace(" ",""); //Se remplazan los espacios con replace
+
+		System.out.println(nueva); //Se imprimer la nueva cadena
+
+
+	}
+
+
+}
